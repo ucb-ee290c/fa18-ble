18c18852291c87a189493d7657982dd7f2f6877f
AlanZheng <alanzhengyl@gmail.com>
2018-10-25 14:30:37 -0700

    new DMA-RF

--- src/main/scala/BLEBaseband/PacketAssembler/DMA_RF_side.scala
+++ src/main/scala/BLEBaseband/PacketAssembler/DMA_RF_side.scala
@@ -9,58 +9,59 @@ class DMA_RF extends Module {
   val io = IO(new Bundle {
 
   	/************* Tx interface ***************/
-    val TL_Tx_Trigger = Input(Bool())
-    val PA_Tx_Trigger = Output(Bool())
+    val tl_tx_trigger = Input(Bool())
+    val pa_tx_trigger = Output(Bool())
 
-    val TL_Tx_fifo = Flipped(Decoupled(UInt(8.W)))
-    val PA_Tx_fifo = Decoupled(UInt(8.W))
+    val tl_tx_data = Flipped(Decoupled(UInt(8.W)))
+    val pa_tx_data = Decoupled(UInt(8.W))
 
-    val TL_Tx_length = Flipped(Irrevocable(UInt(9.W)))
-    val PA_Tx_length = Irrevocable(UInt(8.W))
+    val tl_tx_length = Flipped(Irrevocable(UInt(9.W)))
+    val pa_tx_length = Irrevocable(UInt(8.W))
 
-    val PA_Tx_done = Input(Bool())
-    val TL_Tx_done = Output(Bool())
+    val pa_tx_done = Input(Bool())
+    val tl_tx_done = Output(Bool())
 
     /************* Rx interface ***************/
-    val PA_Rx_Trigger = Input(Bool())
-    val TL_Rx_Trigger = Output(Bool())
+    val pa_rx_trigger = Input(Bool())
+    val tl_rx_trigger = Output(Bool())
 
-    val PA_Rx_fifo = Flipped(Decoupled(UInt(8.W)))
-    val TL_Rx_fifo = Decoupled(UInt(8.W))
+    val pa_rx_data = Flipped(Decoupled(UInt(8.W)))
+    val tl_rx_data = Decoupled(UInt(8.W))
 
-    val PA_Rx_length = Flipped(Irrevocable(UInt(8.W)))
-    val TL_Rx_length = Irrevocable(UInt(9.W))
+    val pa_rx_length = Flipped(Irrevocable(UInt(8.W)))
+    val tl_rx_length = Irrevocable(UInt(9.W))
 
-    val PA_Rx_FlagAA = Flipped(Decoupled(Bool()))
-    val TL_Rx_FlagAA = Decoupled(Bool())
+    val pa_rx_FlagAA = Flipped(Decoupled(Bool()))
+    val tl_rx_FlagAA = Decoupled(Bool())
 
-    val PA_Rx_FlagCRC = Flipped(Decoupled(Bool()))
-    val TL_Rx_FlagCRC = Decoupled(Bool())
+    val pa_rx_FlagCRC = Flipped(Decoupled(Bool()))
+    val tl_rx_FlagCRC = Decoupled(Bool())
 
-    val PA_Rx_done = Input(Bool())
+    val pa_rx_done = Output(Bool())
+    val tl_rx_done = Input(Bool())
 
   })
 
 // data FIFO
-val Txfifo = Module(new Queue(UInt(8.W), 64))
-Txfifo.io.enq <> io.TL_Tx_fifo
-Txfifo.io.deq <> io.PA_Tx_fifo
+val txfifo = Module(new Queue(UInt(8.W), 64))
+txfifo.io.enq <> io.tl_tx_data
+txfifo.io.deq <> io.pa_tx_data
 
-val Rxfifo = Module(new Queue(UInt(8.W), 32))
-Rxfifo.io.enq <> io.PA_Rx_fifo
-Rxfifo.io.deq <> io.TL_Rx_fifo
+val rxfifo = Module(new Queue(UInt(8.W), 32))
+rxfifo.io.enq <> io.pa_rx_data
+rxfifo.io.deq <> io.tl_rx_data
 
-io.PA_Tx_length <> io.TL_Tx_length
+io.pa_tx_length <> io.tl_tx_length
 
-io.PA_Rx_length <> io.TL_Rx_length
-io.TL_Rx_length.bits := io.PA_Rx_length.bits + 4.U + 2.U + 3.U // Access Address and PDU Header
+io.pa_rx_length <> io.tl_rx_length
+io.tl_rx_length.bits := io.pa_rx_length.bits + 4.U + 2.U + 3.U // Access Address and PDU Header
 
-io.PA_Rx_FlagCRC <> io.TL_Rx_FlagCRC
+io.pa_rx_FlagCRC <> io.tl_rx_FlagCRC
 
-io.PA_Rx_FlagAA <> io.TL_Rx_FlagAA
+io.pa_rx_FlagAA <> io.tl_rx_FlagAA
 
-io.PA_Tx_Trigger := io.TL_Tx_Trigger
-io.TL_Rx_Trigger := io.PA_Rx_Trigger
+io.pa_tx_trigger := io.tl_tx_trigger
+io.tl_rx_trigger := io.pa_rx_trigger
 
 // FSM
 val s_Idle :: s_Txdata :: s_Txdone :: s_Rxdata :: Nil = Enum(4)
@@ -70,18 +71,18 @@ val state = Reg(init = s_Idle)
 	{
 		is (s_Idle)
 		{
-			when (io.TL_Tx_Trigger)
+			when (io.tl_tx_trigger)
 			{
 				state := s_Txdata
 			}
-			.elsewhen (io.PA_Rx_Trigger)
+			.elsewhen (io.pa_rx_trigger)
 			{
 				state := s_Rxdata
 			}
 		}
 		is (s_Txdata) // start reading data from FIFO and send to RFController
 		{
-			when (io.PA_Tx_done)
+			when (io.pa_tx_done)
 			{
 				state := s_Txdone
 			}
@@ -92,11 +93,11 @@ val state = Reg(init = s_Idle)
 		}
 		is (s_Rxdata) // start writing data to FIFO from RFController
 		{
-			when (io.PA_Rx_done)
+			when (io.pa_rx_done)
 			{
 				state := s_Idle
 			}
 		}
 	}
-	io.TL_Tx_done := (state === s_Txdone)
+	io.tl_tx_done := (state === s_Txdone)
 }
