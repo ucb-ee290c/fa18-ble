c1d52a00ac52a80ebd91357abad66e3f67034746
AlanZheng <alanzhengyl@gmail.com>
2018-11-13 20:52:18 -0800

    pa.c

--- tests/pa.c
+++ tests/pa.c
@@ -9,15 +9,13 @@
 #include "mmio.h"
 
 
-uint64_t pack_PABundle(uint64_t trigger, uint64_t data, uint64_t crc_seed, uint64_t white_seed, uint64_t done) {
-  return
-    trigger << 40       |
-    data << 32          |
-    crc_seed << 8       |
-    white_seed << 1     |
-    done;
+uint64_t pack_PABundle(uint64_t trigger, uint64_t data) {
+  return (trigger << 8)|data;
 }
 
+uint64_t pack_out(uint64_t in, uint64_t done) {
+    return (done << 1)|in; 
+}
 
 int64_t unpack_trigger(uint64_t packed) {
   return packed[40];
@@ -43,20 +41,39 @@ int64_t unpack_done(uint64_t packed) {
 int main(void)
 {
   uint64_t trigger = 1;
-  uint64_t AA = 01101011011111011001000101110001;
-  uint64_t crc_seed = 010101010101010101010101;
-  uint64_t white_seed = 1100101;
+  uint64_t data1 = 0110101101111101100100010111000101000000000010000110001100000001;
+  uint64_t data2 = 0100110001001110010000000000000001000000100000001010000001100000;
+  uint64_t data3 = 000100001100101011000010101010101011001011001100;
+  //uint64_t crc_seed = 010101010101010101010101;
+  //uint64_t white_seed = 1100101;
   uint64_t done = 0;
   //uint64_t preamble = 01010101;
 
-  uint8_t AA_eight;
-  for (int i = 0; i < 4; i++ ){
-    AA_eight = AA>>8*(3-i);
-    reg_write64(CORDIC_WRITE, pack_PABundle(trigger, AA_eight, crc_seed, white_seed, done));
-    while(reg_read64(CORDIC_READ)){
-    	printf("unpack data: %d", reg_read64(CORDIC_READ)"\n");
+  uint8_t data_eight;
+  uint8_t PA_out;
+  for (int i = 0; i < 22; i++ ){
+    if (i>=0 && i<8) {
+	data_eight = data1>>8*(7-i);
     }
-  }
+    if (i>=8 && i<16) {
+	data_eight = data>>8*(7-(i-8));
+    }
+    if (i>=16 && i<22) {
+	data_eight = data>>8*(7-(i-16));
+    }
+    reg_write64(CORDIC_WRITE, pack_PABundle(trigger, data_eight));
+    }
+   int j=0;
+   while (j<176) {
+	PA_out = reg_read64(CORDIC_READ);
+	if (PA_out%2 == 0) {
+		printf("unpack data: %d", PA_out/2);
+        } else {
+		printf("\n");
+        }
+        j++;
+    }
+  
 
 	return 0;
 }
