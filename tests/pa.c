73d413b7d26de276be47184739b8b520398e1cd0
AlanZheng <alanzhengyl@gmail.com>
2018-11-14 17:14:07 -0800

    pa.c

--- tests/pa.c
+++ tests/pa.c
@@ -17,9 +17,16 @@ uint64_t pack_PABundle(uint64_t trigger, uint64_t data) {
 int main(void)
 {
   uint64_t trigger = 1;
-  uint64_t data1 = 0110101101111101100100010111000101000000000010000110001100000001;
-  uint64_t data2 = 0100110001001110010000000000000001000000100000001010000001100000;
-  uint64_t data3 = 000100001100101011000010101010101011001011001100;
+  uint64_t data_AA = 0x8E89BED6U;
+//01101011011111011001000101110001;
+  uint64_t data_pduH = 0x1002U; 
+//0100000000001000;
+  uint64_t data_pduAA = 0x0002723280C6U;
+//011000110000000101001100010011100100000000000000;
+  uint64_t data_pduData1 = 0x5543530806050102U;
+//0100000010000000101000000110000000010000110010101100001010101010;
+  uint64_t data_pduData2 = 0x334DU;
+//1011001011001100;
   //uint64_t crc_seed = 010101010101010101010101;
   //uint64_t white_seed = 1100101;
   uint64_t done = 0;
@@ -28,16 +35,26 @@ int main(void)
   uint8_t data_eight;
   uint8_t PA_out;
   for (int i = 0; i < 22; i++ ){
-    if (i>=0 && i<8) {
-	data_eight = data1>>8*(7-i);
+    if (i>=0 && i<4) {
+	data_eight = data_AA>>8*i;
+        if (i==0) {
+            reg_write64(CORDIC_WRITE, pack_PABundle(1, 0x00));
+            reg_write64(CORDIC_WRITE, pack_PABundle(0, data_eight));
+        }
     }
-    if (i>=8 && i<16) {
-	data_eight = data2>>8*(7-(i-8));
+    if (i>=4 && i<6) {
+	data_eight = data_pduH>>8*(i-4);
     }
-    if (i>=16 && i<22) {
-	data_eight = data3>>8*(7-(i-16));
+    if (i>=6 && i<12) {
+	data_eight = data_pduAA>>8*(i-6);
     }
-    reg_write64(CORDIC_WRITE, pack_PABundle(trigger, data_eight));
+    if (i>=12 && i<20) {
+        data_eight = data_pduData1>>8*(i-12);
+    }
+    if (i>=20 && i<22) {
+	data_eight = data_pduData2>>8*(i-20);
+    }
+    reg_write64(CORDIC_WRITE, pack_PABundle(0, data_eight)); 
     }
    int j=0;
    while (j<176) {
