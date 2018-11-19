adc30e1c4bebd0469cc7c9765438b4605f9014cf
jerryduan07 <jerry_duan@berkeley.edu>
2018-11-18 17:55:11 -0800

    change c test

--- tests/pa.c
+++ tests/pa.c
@@ -18,22 +18,17 @@ int main(void)
 {
   uint64_t trigger = 1;
   uint64_t data_AA = 0x8E89BED6U;
-//01101011011111011001000101110001;
   uint64_t data_pduH = 0x1002U; 
-//0100000000001000;
   uint64_t data_pduAA = 0x0002723280C6U;
-//011000110000000101001100010011100100000000000000;
-  uint64_t data_pduData1 = 0x5543530806050102U;
-//0100000010000000101000000110000000010000110010101100001010101010;
-  uint64_t data_pduData2 = 0x334DU;
-//1011001011001100;
-  //uint64_t crc_seed = 010101010101010101010101;
-  //uint64_t white_seed = 1100101;
+  uint64_t data_pduData1 = 0x0806050102U; //payload 1
+  uint64_t data_pduData2 = 0x334D554353U; 
+//  uint64_t data_pduData2 = 0x43303932U; //payload 2
+
+
   uint64_t done = 0;
-  //uint64_t preamble = 01010101;
   uint8_t data_eight;
   uint8_t PA_out;
-
+  
   for (int i = 0; i < 22; i++){
     if (i>=0 && i<4) {
 	    data_eight = data_AA>>8*i;
@@ -59,13 +54,13 @@ int main(void)
     printf("pack data: %#010x \n", pack_PABundle(0, data_eight));            
     reg_write64(PACKET_ASSEMBLER_WRITE, pack_PABundle(0, data_eight));
     }
-    if (i>=12 && i<20) {
+    if (i>=12 && i<17) {
     data_eight = data_pduData1>>8*(i-12);
     printf("pack data: %#010x \n", pack_PABundle(0, data_eight));            
     reg_write64(PACKET_ASSEMBLER_WRITE, pack_PABundle(0, data_eight));
     }
-    if (i>=20 && i<22) {
-	data_eight = data_pduData2>>8*(i-20);
+    if (i>=17 && i<22) {
+	data_eight = data_pduData2>>8*(i-17);
     printf("pack data: %#010x \n", pack_PABundle(0, data_eight));            
     reg_write64(PACKET_ASSEMBLER_WRITE, pack_PABundle(0, data_eight));
     }
