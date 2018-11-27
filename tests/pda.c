059ea317ed321b281be0b8a1a82f5b3f041bc7b3
jerryduan07 <jerry_duan@berkeley.edu>
2018-11-27 15:12:22 -0800

    pda c test; preamble and aa working

--- tests/pda.c
+++ tests/pda.c
@@ -18,7 +18,7 @@ uint64_t convertToHex(uint64_t number) {
   uint64_t hexResult = 0;
   uint64_t hexChar[16]={0x0,0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8,0x9,0xa,0xb,0xc,0xd,0xe,0xf}; 
   while(number) {
-      hexResult = hexChar[number & 0xf] + hexResult << 4;
+      hexResult = hexChar[number & 0xf] << 4 + hexResult;
       number = (unsigned) number >> 4;
 }
   return hexResult;
@@ -29,40 +29,39 @@ int main(void)
     //input: pre_preamble bpreamble baccess_address pdu_crc_whitened
   uint64_t trigger = 1;
   uint64_t data_pre_preamble = 000111U;
-  uint64_t data_preamble = 01010101U;//2*4
-  uint64_t data_AA = 01101011011111011001000101110001U;//8*4
-  uint64_t data_pdu_crc_whiten = 1111000110111011100010011000010011110000101010110010011000001101111011100000110000101000011100100111100110100010100000111100101110100000110001001110110011101011U;//40*4
+  uint64_t data_preamble = 0x55U;//2*4
+  uint64_t data_AA = 0X6B7D9171U;
+  uint64_t data_pdu_crc_whiten = 1111 0001 1011 1011 1000 1001 1000 0100 1111 0000 1010 1011 0010 0110 0000 1101 1110 1110 0000 1100 0010 1000 0111 0010 0111 1001 1010 0010 1000 0011 1100 1011 1010 0000 1100 0100 1110 1100 1110 1011U;//40*4
+  
 
-  //output:
-  //uint64_t done = 0;
   uint8_t data;
   uint8_t PDA_out;
 
   for (int i = 0; i < 50; i++){
-
-    if (i>=0 && i<2) {
-        data = (convertToHex(data_preamble)>>(7-i)) & 1;
-        if (i==0) {
+    if (i >= 0 && i < 8){
+        data = (data_preamble >> (7-i)) & 1;
+        if (i == 0) {
             printf("pack data: %#010x \n", pack_PDABundle(1, data));
-            reg_write8(PACKET_DISASSEMBLER_WRITE, pack_PDABundle(1, data));
+            reg_write64(PACKET_DISASSEMBLER_WRITE, pack_PDABundle(1, data));
             printf("pack data: %#010x \n", pack_PDABundle(0, data));
-            reg_write8(PACKET_DISASSEMBLER_WRITE, pack_PDABundle(0, data));
+            reg_write64(PACKET_DISASSEMBLER_WRITE, pack_PDABundle(0, data));
         }
         else {
             printf("pack data: %#010x \n", pack_PDABundle(0, data));
             reg_write64(PACKET_DISASSEMBLER_WRITE, pack_PDABundle(0, data));
         }
     }
-    if (i>=2 && i<10) {
-	data = (convertToHex(data_AA)>>(9-i+2)) & 1;
-    printf("pack data: %#010x \n", pack_PDABundle(0, data));
-    reg_write64(PACKET_DISASSEMBLER_WRITE, pack_PDABundle(0, data));
+    if (i >= 8 && i < 40) {
+	    data = (data_AA >> (39-i)) & 1;
+        printf("pack data: %#010x \n", pack_PDABundle(0, data));
+        reg_write64(PACKET_DISASSEMBLER_WRITE, pack_PDABundle(0, data));
     }
+    /*
     if (i>=10 && i<50) {
-	data = (convertToHex(data__pdu_crc_whiten)>>(49-i+10)) & 1;
+	data = (convertToHex(data_pdu_crc_whiten)>>(49-i+10)) & 1;
     printf("pack data: %#010x \n", pack_PDABundle(0, data));
     reg_write64(PACKET_DISASSEMBLER_WRITE, pack_PDABundle(0, data));
-    }
+    }*/
 
   }
 
